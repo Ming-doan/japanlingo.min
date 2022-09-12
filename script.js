471ad7e081d47f1a7dcd7ea150c1d558c87d16ca
Ming-doan <quangminh57dng@gmail.com>
2022-09-12 20:36:58 +0700

    Separate libs

--- script.js
+++ script.js
@@ -1,57 +1,6 @@
 // Global Variables
 
-const LIBRARY = [
-    {
-        hiragana: 'あ',
-        katakana: 'ア',
-        romaji: 'a'
-    },
-    {
-        hiragana: 'い',
-        katakana: 'イ',
-        romaji: 'i'
-    },
-    {
-        hiragana: 'う',
-        katakana: 'ウ',
-        romaji: 'u'
-    },
-    {
-        hiragana: 'え',
-        katakana: 'エ',
-        romaji: 'e'
-    },
-    {
-        hiragana: 'お',
-        katakana: 'オ',
-        romaji: 'o'
-    },
-    {
-        hiragana: 'か',
-        katakana: 'カ',
-        romaji: 'ka'
-    },
-    {
-        hiragana: 'き',
-        katakana: 'キ',
-        romaji: 'ki'
-    },
-    {
-        hiragana: 'く',
-        katakana: 'ク',
-        romaji: 'ku'
-    },
-    {
-        hiragana: 'け',
-        katakana: 'ケ',
-        romaji: 'ke'
-    },
-    {
-        hiragana: 'こ',
-        katakana: 'コ',
-        romaji: 'ko'
-    },
-]
+import LIBRARY from "./libs"
 
 const LIBRARY_LENGTH = LIBRARY.length
 
