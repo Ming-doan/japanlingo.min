1ffc19829694010fa4bfd4cfce0924a0713dafe7
Ming-doan <quangminh57dng@gmail.com>
2022-09-12 20:45:08 +0700

    Fixbug

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
 // Global Variables
 
-import LIBRARY from "./libs"
+const LIBRARY = [
+    {
+        hiragana: 'あ',
+        katakana: 'ア',
+        romaji: 'a'
+    },
+    {
+        hiragana: 'い',
+        katakana: 'イ',
+        romaji: 'i'
+    },
+    {
+        hiragana: 'う',
+        katakana: 'ウ',
+        romaji: 'u'
+    },
+    {
+        hiragana: 'え',
+        katakana: 'エ',
+        romaji: 'e'
+    },
+    {
+        hiragana: 'お',
+        katakana: 'オ',
+        romaji: 'o'
+    },
+    {
+        hiragana: 'か',
+        katakana: 'カ',
+        romaji: 'ka'
+    },
+    {
+        hiragana: 'き',
+        katakana: 'キ',
+        romaji: 'ki'
+    },
+    {
+        hiragana: 'く',
+        katakana: 'ク',
+        romaji: 'ku'
+    },
+    {
+        hiragana: 'け',
+        katakana: 'ケ',
+        romaji: 'ke'
+    },
+    {
+        hiragana: 'こ',
+        katakana: 'コ',
+        romaji: 'ko'
+    },
+]
 
 const LIBRARY_LENGTH = LIBRARY.length
 
